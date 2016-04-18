e2fd1e83298bd3765de541e70bc469d5737a699b
FND <fnd@localhost.localdomain>
2016-04-18 06:10:50 +0200

    extended debounce to support custom execution context

--- debounce.js
+++ debounce.js
@@ -1,15 +1,19 @@
 // adapted from StuffJS <https://github.com/bengillies/stuff-js>
-module.exports = function(delay, fn) {
+module.exports = function(delay, ctx, fn) {
+	if(fn === undefined) { // shift arguments
+		fn = ctx;
+		ctx = null;
+	}
+
 	var timer;
 	return function() {
-		var self = this;
 		var args = arguments;
 		if(timer) {
 			clearTimeout(timer);
 			timer = null;
 		}
 		timer = setTimeout(function() {
-			fn.apply(self, args);
+			fn.apply(ctx, args);
 			timer = null;
 		}, delay);
 	};
