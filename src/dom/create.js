7f6333c5dbfd3124cd575ce6d4d94e695975faa9
FND <fnd@localhost.localdomain>
2017-04-09 15:28:25 +0200

    simplified `createElement` signature
    this happens to match the convention established by JSX (which is also
    why we didn't just use a default parameter value for `params`, as we
    want to treat `null` like `undefined` there)

--- src/dom/create.js
+++ src/dom/create.js
@@ -3,15 +3,9 @@
 // respective type (string or boolean attributes vs. arbitrary properties)
 // if a `ref` parameter is provided, it is expected to contain a `[refs, id]`
 // tuple; `refs[id]` will be assigned the respective DOM node
-// `children` is an array of strings or DOM elements - for convenience, the
-// array may be omitted when passing only a single entity
-// adapted from <https://github.com/roca-components/declarative-rendering>
-export function createElement(tag, params, children = []) {
+// `children` is an array of strings or DOM elements
+export function createElement(tag, params, ...children) {
 	params = params || {};
-	if(!children.pop) {
-		children = [children];
-	}
-
 	let node = document.createElement(tag);
 	Object.keys(params).forEach(param => {
 		let value = params[param];
