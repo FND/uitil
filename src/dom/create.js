222d673536608dd1c7bf39189c1a7fdb97196386
FND <fnd@localhost.localdomain>
2018-06-19 12:20:25 +0200

    added DOM element creation via tagged templates

--- src/dom/create.js
+++ src/dom/create.js
@@ -38,3 +38,20 @@ export function createElement(tag, params, ...children) {
 
 	return node;
 }
+
+// tag expression for HTML template literals
+// NB:
+// * returns a single DOM element
+// * discards blank values (`undefined`, `null`, `false`) to allow for
+//   conditionals with boolean operators (`condition && value`)
+export function html2dom(parts, ...values) {
+	let html = parts.reduce((memo, part, i) => {
+		let val = values[i];
+		let blank = val === undefined || val === null || val === false;
+		return memo.concat(blank ? [part] : [part, val]);
+	}, []).join("");
+
+	let tmp = document.createElement("div");
+	tmp.innerHTML = html.trim();
+	return tmp.childNodes[0];
+}
